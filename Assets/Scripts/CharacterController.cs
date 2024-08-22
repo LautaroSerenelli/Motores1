0c3c199e0edc6e0716df0290acad5ffc3c041c5a
LautaroSerenelli <lauti.serenelli@gmail.com>
2024-08-22 13:26:49 -0300

    Cambios Script CharacterController
    Movimiento rehecho desde 0.
    Mejora de la aceleración y desaceleración.
    Salto terminado.

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -5,51 +5,105 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
+    public Vector3 Direccion;
     public float Velocidad;
-    public float VelocidadMin;
-    public float VelocidadMax;
+    private Vector3 VelocidadObjetivo = Vector3.zero;
+    //test
+    public float VelocidadActualX;
+    public float VelocidadActualZ;
+
     public float Aceleracion;
     public float Desaceleracion;
 
+    public float FuerzaSalto;
+    public bool EnSuelo = false;
+    // public float FuerzaGravedad;
+
+    public LayerMask CapaSuelo;
+    
     private Rigidbody rb;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        // rb.useGravity = false; //Desactiva la gravedad global para este rb
     }
 
     private void Update()
     {
-        float movHorizontal = Input.GetAxis("Horizontal");
-        float movVertical = Input.GetAxis("Vertical");
+        Movimiento();
+    }
 
-        Vector3 movimiento = new Vector3(movHorizontal, 0.0f, movVertical);
-        
-        //Aceleracion
-        if (movHorizontal != 0 || movVertical != 0)
+    private void FixedUpdate()
+    {
+        if (Direccion != Vector3.zero)
         {
-            rb.AddForce(movimiento * Aceleracion);
-
-            //Limitar la velocidad max
-            if (rb.velocity.magnitude > VelocidadMax)
-            {
-                rb.velocity = rb.velocity.normalized * VelocidadMax;
-            }
+            //Aceleracion
+            VelocidadObjetivo = new Vector3(Direccion.x * Velocidad, rb.velocity.y, Direccion.z * Velocidad);
+            rb.velocity = Vector3.Lerp(rb.velocity, VelocidadObjetivo, Time.deltaTime * Aceleracion);
         }
         else
         {
-            if (rb.velocity.magnitude >= VelocidadMin)
+            //Desaceleracion
+            float xVel = rb.velocity.x;
+            float zVel = rb.velocity.z;
+
+            if (Direccion.x == 0)
             {
-                Vector3 desacelerar = rb.velocity.normalized * Mathf.Max(rb.velocity.magnitude - Desaceleracion * Time.fixedDeltaTime, VelocidadMin);
-                rb.velocity = Vector3.Lerp(rb.velocity, desacelerar, Time.fixedDeltaTime * Desaceleracion);
+                xVel = Mathf.Lerp(xVel, 0, Time.deltaTime * Desaceleracion);
             }
-            else
+            if (Direccion.z == 0)
             {
-                rb.velocity = Vector3.zero;
+                zVel = Mathf.Lerp(zVel, 0, Time.deltaTime * Desaceleracion);
             }
+
+            if (xVel < 0.7f && xVel > -0.5f)
+            {
+                xVel = 0f;
+            }
+            if (zVel < 0.7f && zVel > -0.5f)
+            {
+                zVel = 0f;
+            }
+
+            VelocidadObjetivo = new Vector3(xVel, rb.velocity.y, zVel);
+            rb.velocity = VelocidadObjetivo;
         }
 
-        //Visualizar en el inspector la velocidad
-        Velocidad = rb.velocity.magnitude;
+        //test
+        VelocidadActualX = rb.velocity.x;
+        VelocidadActualZ = rb.velocity.z;
+    }
+
+    private void Movimiento()
+    {
+        Direccion = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Direccion = Direccion.normalized;
+
+        if (Input.GetButtonDown("Jump") && EnSuelo)
+        {
+            Salto();
+        }
+    }
+
+    private void Salto()
+    {
+        rb.velocity += Vector3.up * FuerzaSalto;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.layer == LayerMask.NameToLayer("Suelo"))
+        {
+            EnSuelo = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if(collision.gameObject.layer == LayerMask.NameToLayer("Suelo"))
+        {
+            EnSuelo = false;
+        }
     }
 }
